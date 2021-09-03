64e44a1c58492320e327a171b186102b901208c9
Fabio Murer <murer.fabio23@gmail.com>
2021-09-03 14:08:17 +0200

    sis

--- scripts/s4.js
+++ scripts/s4.js
@@ -1,9 +1,22 @@
-function summary() {
-    let summaryContent = 'ATHLETE\n' + sessionStorage.getItem('athlete') + '\nANSWERS\n' + sessionStorage.getItem('raceAnswers')
-    document.getElementById("summary").innerHTML = summaryContent;
+let summary = '';
+
+function showSummary() {
+    let athlete = JSON.parse(sessionStorage.getItem('athlete'));
+    let raceAnswers = JSON.parse(sessionStorage.getItem('raceAnswers'));
+    let jsonAnswers = { "athlete": athlete, "time": sessionStorage.getItem('time'), "answers": raceAnswers };
+    summary = JSON.stringify(jsonAnswers);
+    document.getElementById("summary").innerHTML = summary;
 }
 
 function exitRace() {
     sessionStorage.clear();
     document.location.href = 'p1.html';
+}
+
+function download() {
+    let dataurl = 'data:text,' + summary;
+    var a = document.createElement("a");
+    a.href = dataurl;
+    a.setAttribute("download", 'answers.json');
+    a.click();
 }
