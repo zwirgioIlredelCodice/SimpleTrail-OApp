3dde29d0b6a3796a454ca6e3a32875896d54ea80
Fabio Murer <murer.fabio23@gmail.com>
2021-09-04 14:05:19 +0200

    lulsdfsdfsd

--- app.js
+++
@@ -1,9 +0,0 @@
-if ('ServiceWorker' in navigator) {
-  navigator.serviceWorker.register('mobileTempO/sw.js').then(registration => {
-    console.log("SW Registered");
-    console.log(registration);
-  }).catch(error => {
-    console.log("SW regisdtration failed");
-    console.log(error);
-  });
-}
