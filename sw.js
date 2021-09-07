b4014ce9995b6a32ba5e2e1b718aecb3574d39eb
Fabio Murer <murer.fabio23@gmail.com>
2021-09-07 17:01:27 +0200

    asdasd

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'v8';
+const staticCacheName = 'v8.1';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -12,7 +12,6 @@ self.addEventListener('install', function(event) {
           '/mobileTempO/pages/p2.html',
           '/mobileTempO/pages/p3.html',
           '/mobileTempO/pages/p4.html',
-          '/mobileTempO/scripts/',
           '/mobileTempO/scripts/s1.js',
           '/mobileTempO/scripts/s2.js',
           '/mobileTempO/scripts/s3.js',
