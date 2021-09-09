75a9bdc6d749a4a6c91e13de8ccad3295ce80b42
Fabio Murer <murer.fabio23@gmail.com>
2021-09-09 16:52:50 +0200

    rewrite s2, s3 using createElement() and setattribute() instead of innerHTML

--- scripts/s3.js
+++ scripts/s3.js
@@ -3,31 +3,35 @@ let timeElapsed = 0;
 let timerID = -1;
 
 function readRace() {
-    var form = document.createElement("FORM");   // Create a <form> element
-    form.setAttribute("id", "pointForm");
-
-    let pointForm = '<div class="tocenter">';
+    let div1 = document.getElementById('div1');
 
     for (let i = 0; i < jsonData.event.controlpoints.length; i++) {
-
-        pointForm = pointForm + '<p>point ' + (i + 1) + '</p><br>';
-
-        pointForm = pointForm + '<div class="tocolor">';
+        
+        let p = document.createElement('p');
+        p.innerText = 'point ' + (i+1);
+        div1.appendChild(p);
+        
+        let div2 = document.createElement('div');
+        div2.setAttribute('class','tocolor');
+        
         for (let y = 0; y < jsonData.event.controlpoints[i].length; y++) {
             let point = jsonData.event.controlpoints[i][y];
-            pointForm = pointForm + '<input type="radio" id="' + point + '" name="point' + i + '" value="' + point + '">\n' + '<label for="' + point + '">' + point + '</label>\n';
+            let input = document.createElement('input')
+            input.setAttribute('type', 'radio');
+            input.setAttribute('id', point);
+            input.setAttribute('name', ('point'+i));
+            input.setAttribute('value', point);
+            div2.appendChild(input);
 
+            let label  = document.createElement('label');
+            label.setAttribute('for', point);
+            label.innerText = point;
+            div2.appendChild(label);
         }
-        pointForm = pointForm + '</div>';
-
-        form.innerHTML = pointForm;
-        if (document.getElementById('pointForm') == null) { //if this element is not created yet
-            document.body.appendChild(form);
-        }
+        div1.appendChild(div2);
     }
-    pointForm = pointForm + '</div>'
     timeStart();
-}
+} 
 
 function finishRace() {
     timeStop();
